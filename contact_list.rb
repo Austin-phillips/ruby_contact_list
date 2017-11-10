616f3a3b916e8df2b8475ebb1f9e0a9cb5640fe3
Austin Phillips <phillips.austin51@gmail.com>
2017-11-10 12:03:32 -0700

    user choice and menu working

--- contact_list.rb
+++ contact_list.rb
@@ -5,3 +5,16 @@ puts "Welcome to the contact list"
 puts "1) Show Contacts"
 puts "2) Add Contact"
 puts "3) Quit"
+
+user_input = gets.to_i
+case user_input
+  when 1
+    puts "Show all contacts"
+  when 2 
+    puts "Add new contact"
+  when 3
+    puts "Thanks for contact listing"
+    exit
+  else 
+    puts "Bad input"
+end
